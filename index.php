f8dc20b466670892cc2fef7368959cc7cd548570
danielito-yagami <37171831+danielito-yagami@users.noreply.github.com>
2018-03-08 09:30:21 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+<title>Bienvenido</title>
+</head>
+<body>
+<h2>--------PAGINA DE EJEMPLO--------</h2>
+
+
+<p>MISION</p>
+<br>
+<p>Alimentos deliciosos y nutritivos en las manos de todos.</p>
+<br>
+<p>VISION</p>
+<p>En 2020 transformamos la industria de la panificación y expandimos nuestro liderazgo global para servir mejor a los consumidores.
+
+“Somos una empresa sustentable, altamente productiva y plenamente humana”</p>
+<br>
+<p>Valoramos a la persona
+
+La seguridad de nuestros colaboradores va por delante.
+Seguimos nuestra regla de oro tratándonos con respeto, justicia, confianza y afecto. Todos tenemos potencial: fomentamos el desarrollo integral de nuestros colaboradores. La diversidad nos enriquece, la inclusión nos fortalece.
+Somos una comunidad
+
+Somos una misma empresa global en cualquier lugar.
+Nos apasiona lo que hacemos, y se nota. Colaboramos para romper silos. Hacemos el trabajo divertido y celebramos nuestros logros.
+Conseguimos resultados
+
+Pensamos como dueños, cuidando el hoy y el mañana. Empoderamos, pedimos y rendimos cuentas. Somos ágiles descubriendo y capturando oportunidades.
+Competimos y ganamos
+
+Somos un equipo con hambre y garra, nunca satisfecho y con sentido de urgencia.
+El consumidor es nuestro jefe, el cliente es nuestro socio. Construimos y cultivamos marcas que conectan con los consumidores.
+Somos operadores eficaces
+
+Dominan nuestro oficio, ejecutando con precisión y excelencia. No aceptamos el desperdicio ni el gasto innecesario. Mejoramos y aprendemos todos los días. Valoramos la simplicidad y adoptamos nuevas tecnologías.
+Actuamos con integridad
+
+Pensamos, decidimos y hacemos lo correcto. Actuamos siempre cuidando el interés del Grupo Bimbo. Valoramos la buena voluntad del otro.
+Transcendemos y permanecemos en el tiempo
+
+La rentabilidad es el oxígeno de la empresa. Lideramos el mercado, anticipando y adaptándonos al cambio. Somos una fuerza de bien en la vida de nuestros colaboradores, nuestra sociedad y nuestro planeta.</p>
+
+<a href="registro.php">IR AL REGISTRO</a>
+</body>
+
+</html>
---
+++ registro.php
@@ -0,0 +1,47 @@
+<html>
+<head>
+<title>Bienvenido</title>
+</head>
+<body>
+<h2>--------Registro--------</h2>
+
+<p>Por favor llena los campos que se te piden :)</p>
+
+<form action="/formulario" method="post">
+<div>
+<label for="name">Nombre:</label>
+<input type="text" id="name" />
+</div>
+
+<div>
+<label for="apPaterno">Apellido Paterno:</label>
+<input type="text" id="apPAterno"/>
+</div>
+
+<div>
+<label for="apMaterno">Apellido Materno:</label>
+<input type="text" id="apMaterno"/>
+</div>
+
+
+<div>
+<label for="phone">Telefono:</label>
+<input type="phone" id="phone"/>
+</div>
+
+
+<div>
+<label for="mail">E-mail:</label>
+<input type="email" id="correo" />
+</div>
+
+<div class="button">
+
+<button type="submit">ACEPTAR ENVIAR DATOS</button>
+</div>
+</form>
+
+
+</body>
+
+</html>
